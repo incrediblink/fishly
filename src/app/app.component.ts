fa154ac111112d9a1251ae1c8b580299f0181c74
IncredibLink <incrediblelink@gmail.com>
2017-03-31 22:52:42 +0800

    光鱼姐姐的门面，我的艺术人生

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  parts = [{
+    borderColor: '#CF000F',
+    text: '痴',
+    class: 'silly'
+  }, {
+    borderColor: '#19B5FE',
+    text: '言',
+    class: 'wordy'
+  }, {
+    borderColor: '#DADFE1',
+    text: '黑',
+    class: 'black'
+  }, {
+    borderColor: '#ffff00',
+    text: '城',
+    class: 'complex'
+  }]
 }
